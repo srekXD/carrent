901ef3286e02d4ca958821dc9463768d62bb83d0
Svetlana Georgieva <sv.georgieva@pgzaimov.com>
2023-02-23 09:35:25 +0200

    Add project files.

---
+++ carrent/Data/CarDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace carrent.Data
+{
+    public class CarDbContext : IdentityDbContext<Clieunt>
+    {
+        public CarDbContext(DbContextOptions<CarDbContext> options)
+            : base(options)
+        {
+        }
+        public DbSet<Car> Cars;
+        public DbSet<Rezervation> Rezerations;
+        public DbSet<BrandModel> Brands;
+    }
+}
